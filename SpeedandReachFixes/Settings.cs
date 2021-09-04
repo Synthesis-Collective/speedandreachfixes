78345882530e74d39b55a172128d015f1f729cc6
radj307 <radj@hotmail.ca>
2021-09-03 21:01:02 -0400

    Finalized settings structure

--- SpeedandReachFixes/Settings.cs
+++ SpeedandReachFixes/Settings.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using Mutagen.Bethesda;
 using Mutagen.Bethesda.FormKeys.SkyrimSE;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
 using Noggog;
 
@@ -18,19 +20,99 @@ namespace SpeedandReachFixes
         public const int DefaultPriority = -1; // default priority returned when no matches were found
     }
 
+	/**
+	 * @class GameSettingsWeaponTypeReach
+	 * @brief Contains game settings related to weapon type reach modifiers, specifically fObjectHitWeaponReach, fObjectHitTwoHandReach, & fObjectHitH2HReach.
+	 */
+	public class GameSettingsWeaponTypeReach {
+        [MaintainOrder]
+
+        [Tooltip("Enables this category. It is highly recommended that you leave this enabled!")] 
+        public bool Enabled = true;
+
+        [Tooltip("Modifier applied to the reach of one-handed weapons.")]
+        public float fObjectHitWeaponReach = 81F;
+
+        [Tooltip("Modifier applied to the reach of two-handed weapons.")]
+        public float fObjectHitTwoHandReach = 135F;
+
+        [Tooltip("Modifier applied to unarmed reach.")]
+        public float fObjectHitH2HReach = 61F;
+
+		// Adds the game settings from this class to the current patcher state
+		public int AddGameSettings(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            if (!Enabled) return 0;
+            state.PatchMod.GameSettings.Add(new GameSettingFloat(state.PatchMod.GetNextFormKey(), state.PatchMod.SkyrimRelease)
+            {
+                EditorID = "fObjectHitWeaponReach",
+                Data = fObjectHitWeaponReach
+            });
+            state.PatchMod.GameSettings.Add(new GameSettingFloat(state.PatchMod.GetNextFormKey(), state.PatchMod.SkyrimRelease)
+            {
+                EditorID = "fObjectHitTwoHandReach",
+                Data = fObjectHitTwoHandReach
+            });
+            state.PatchMod.GameSettings.Add(new GameSettingFloat(state.PatchMod.GetNextFormKey(), state.PatchMod.SkyrimRelease)
+            {
+                EditorID = "fObjectHitH2HReach",
+                Data = fObjectHitH2HReach
+            });
+            return 3;
+        }
+    }
+
+	/**
+	 * @class GameSettingsCombatReach
+	 * @brief Contains game settings related to base reach multipliers, specifically fCombatDistance and fCombatBashReach.
+	 */
+    public class GameSettingsCombatReach {
+        [MaintainOrder]
+
+        [Tooltip("Enables this category. It is highly recommended that you leave this enabled!")]
+        public bool Enabled = true;
+
+        [Tooltip("Melee Range Formula: ( range = fCombatDistance * NPCRaceScale * WeaponReach + { WeaponTypeReach } )")]
+        public float fCombatDistance = 141F;
+
+        [Tooltip("Same as above, but for shield bashing.")]
+        public float fCombatBashReach = 61F;
+
+		// Adds the game settings from this class to the current patcher state
+        public int AddGameSettings(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            if (!Enabled) return 0;
+			state.PatchMod.GameSettings.Add( new GameSettingFloat( state.PatchMod.GetNextFormKey(), state.PatchMod.SkyrimRelease ) {
+				EditorID = "fCombatDistance",
+				Data = fCombatDistance
+			} );
+			state.PatchMod.GameSettings.Add( new GameSettingFloat( state.PatchMod.GetNextFormKey(), state.PatchMod.SkyrimRelease ) {
+				EditorID = "fCombatBashReach",
+				Data = fCombatBashReach
+			} );
+            return 2;
+        }
+    }
+
     /**
      * @class GameSettings
      * @brief Contains settings related to GMST (Game Setting) records.
      */
     public class GameSettings
     {
-        [SettingName("Fix Shield Bash Reach")] public bool CombatBashReach = true;
+        [MaintainOrder]
+        [SettingName("Weapon Type Reach Modifiers"), Tooltip("Melee Reach Formula: ( reach = { fCombatDistance | fCombatBashReach } * NPCRaceScale * WeaponReach + { fObjectHitWeaponReach | fObjectHitTwoHandReach | fObjectHitH2HReach } )")]
+        public GameSettingsWeaponTypeReach WeaponTypeReach = new();
+        [SettingName("Base Combat Reach"), Tooltip("Melee Reach Formula: ( reach = { fCombatDistance | fCombatBashReach } * NPCRaceScale * WeaponReach + { fObjectHitWeaponReach | fObjectHitTwoHandReach | fObjectHitH2HReach } )")]
+        public GameSettingsCombatReach CombatReach = new();
 
-        [SettingName("Fix Combat Distance")] public bool CombatDistance = true;
-
-        [SettingName("Fix Object Reach")] [Tooltip("Changes how far away weapons can hit objects.")] public bool ObjectHitWeaponReach = true;
-
-        [MaintainOrder] [SettingName("[EXPERIMENTAL] Change Swing Angle for all races.")] [Tooltip("Don't change this unless you know what you're doing!")] public bool WeaponSwingAngleChanges = false;
+		public int AddGameSettingsToPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+		{
+			var count = 0;
+			count += WeaponTypeReach.AddGameSettings( state ); // add game settings from weapon type reach category
+			count += CombatReach.AddGameSettings( state ); // add game settings from combat reach category
+			return count;
+		}
     }
 
     /**
@@ -39,17 +121,22 @@ namespace SpeedandReachFixes
      */
     public class WeaponStats
     {
-        [SettingName("Modifier")] [Tooltip("When checked, adds the specified values rather than overwriting them. Negative values will subtract.")] public bool IsModifier;
+		[MaintainOrder]
 
-        // VARS
+		[Tooltip( "The keyword attached to this weapon type." )]
+		public FormLink<IKeywordGetter>? Keyword;
 
-        [MaintainOrder] [Tooltip("The keyword attached to this weapon type.")] public FormLink<IKeywordGetter>? Keyword;
+		[Tooltip( "When multiple weapon types apply to the same category, the highest priority wins." )]
+		public int Priority;
 
-        [Tooltip("When multiple weapon types apply to the same category, the highest priority wins.")] public int Priority;
+		[SettingName("Modifier"), Tooltip("When checked, adds the specified values rather than overwriting them. Negative values will subtract.")] 
+		public bool IsModifier;
 
-        [Tooltip("The range of this weapon. A modifier value of 0 means unchanged.")] public float Reach;
+        [Tooltip("The range of this weapon. A modifier value of 0 means unchanged.")] 
+		public float Reach;
 
-        [Tooltip("The speed of this weapon. A modifier value of 0 means unchanged.")] public float Speed;
+        [Tooltip("The speed of this weapon. A modifier value of 0 means unchanged.")] 
+		public float Speed;
 
         // Default Constructor
         public WeaponStats()
@@ -71,19 +158,19 @@ namespace SpeedandReachFixes
             Speed = speed;
         }
 
-        // FUNCTIONS
-
+		// Private function that compares, modifies, and returns weapon stat values.
         private float GetFloat(float current, float local, out bool changed)
         {
             changed = !local.Equals(current) && !local.Equals(Constants.NullFloat);
             return changed ? IsModifier ? current + local : local : current;
         }
 
+		// Modify a weapon reach value.
         public float GetReach(float current, out bool changed)
         {
             return GetFloat(current, Reach, out changed);
         }
-
+		// Modify a weapon speed value.
         public float GetSpeed(float current, out bool changed)
         {
             return GetFloat(current, Speed, out changed);
@@ -96,18 +183,32 @@ namespace SpeedandReachFixes
                 return Priority;
             return Constants.DefaultPriority;
         }
-
+		// Returns true if the keyword is not set, or the reach and speed values are both unset.
         public bool IsNull()
         {
             return (Keyword == null) || (Reach.Equals(Constants.DefaultPriority) && Speed.Equals(Constants.DefaultPriority));
         }
     }
 
+	/**
+	 * @class Settings
+	 * @brief Contains all settings used throughout the patcher. This is the object passed to SetAutoGeneratedSettings.
+	 */
     public class Settings
     {
-        [SettingName("Game Setting Changes")] [Tooltip("Don't change these unless you know what you're doing!")] public GameSettings GameSettings = new();
+        [MaintainOrder]
 
-        [MaintainOrder] [SettingName("Weapon Groups")] [Tooltip("Change the stats of each weapon group.")] public List<WeaponStats> WeaponStats = new() {
+		// Game Settings
+        [SettingName("Game Setting Changes"), Tooltip("Don't change these unless you know what you're doing!")] 
+        public GameSettings GameSettings = new();
+
+		// Race Attack Angle
+        [SettingName("Experimental Race Strike Angle Modifier"), Tooltip("Changes the angle at which an NPC can be hit by an attack. This value is a modifier and is added to the current value for all attacks and races. Set to 0 to disable.")]
+        public float AttackStrikeAngleModifier = 7F;
+
+		// List of WeaponStats objects, each relating to a different weapon keyword.
+        [SettingName("Weapon Groups"), Tooltip("Change the stats of each weapon group.")] 
+        public List<WeaponStats> WeaponStats = new() {
             new WeaponStats(1, false, Skyrim.Keyword.WeapTypeBattleaxe, 0.666667F, 0.8275F),
             new WeaponStats(1, false, Skyrim.Keyword.WeapTypeDagger, 1.35F, 0.533F),
             new WeaponStats(1, false, Skyrim.Keyword.WeapTypeGreatsword, 0.85F, 0.88F),
@@ -124,13 +225,20 @@ namespace SpeedandReachFixes
             new WeaponStats(2, true, NewArmoury.Keyword.WeapTypeRapier, Constants.NullFloat, 0.2F),
             new WeaponStats(2, true, NewArmoury.Keyword.WeapTypeSpear),
             new WeaponStats(2, true, NewArmoury.Keyword.WeapTypeWhip, Constants.NullFloat, 0.5F),
-            // TODO: Find appropriate value for NWTA & WotM unarmed weapons
+            // TODO: Find appropriate values for NWTA & WotM unarmed weapons
             new WeaponStats(2, true, NWTA.Keyword.WeapTypeKatana),
             new WeaponStats(2, true, NWTA.Keyword.WeapTypeCurvedSword),
             new WeaponStats(2, true, WayOfTheMonk.Keyword.WeapTypeUnarmed)
         };
 
-        public WeaponStats GetHighestPriorityStats(Weapon weapon)
+		// Modify an attack angle by adding the current AttackStrikeAngleModifier value to it.
+        public float GetModifiedStrikeAngle(float current)
+        {
+            return current += AttackStrikeAngleModifier;
+        }
+
+		// Private function that retrieves the highest priority applicable WeaponStats instance from the current settings
+        private WeaponStats GetHighestPriorityStats(Weapon weapon)
         {
             WeaponStats highestStats = new();
             var highest = 0;
@@ -143,7 +251,8 @@ namespace SpeedandReachFixes
 
             return highestStats;
         }
-
+		
+		// Applies the current weapon stats configuration to a given weapon ref.
         public bool ApplyChangesToWeapon(Weapon weapon)
         {
             var stats = GetHighestPriorityStats(weapon);
