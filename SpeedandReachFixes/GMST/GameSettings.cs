adba738ede28ebcd6f14e18cbf613ba760ad8d3b
radj307 <radj@hotmail.ca>
2021-09-06 20:42:24 -0400

    switched to default formatting

--- SpeedandReachFixes/GMST/GameSettings.cs
+++ SpeedandReachFixes/GMST/GameSettings.cs
@@ -2,25 +2,29 @@ using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
 
-namespace SpeedandReachFixes.GMST {
-	/// <summary>
-	/// Contains settings related to GMST (Game Setting) records.
-	/// </summary>
-	public class GameSettings {
-		[MaintainOrder]
-		// Combat reach
-		[SettingName( "Base Combat Reach Multipliers" ), Tooltip( "Melee Reach Formula: ( reach = ( fCombatDistance | fCombatBashReach ) * NPCRaceScale * WeaponReach + ( fObjectHitWeaponReach | fObjectHitTwoHandReach | fObjectHitH2HReach ) )" )]
-		public GameSettingsCombatReach CombatReach = new();
-		// Weapon type reach
-		[SettingName( "Weapon Type Reach Modifiers" ), Tooltip( "Melee Reach Formula: ( reach = ( fCombatDistance | fCombatBashReach ) * NPCRaceScale * WeaponReach + ( fObjectHitWeaponReach | fObjectHitTwoHandReach | fObjectHitH2HReach ) )" )]
-		public GameSettingsWeaponTypeReach WeaponTypeReach = new();
+namespace SpeedandReachFixes.GMST
+{
+    /// <summary>
+    /// Contains settings related to GMST (Game Setting) records.
+    /// </summary>
+    public class GameSettings
+    {
+        [MaintainOrder]
+        // Combat reach
+        [SettingName("Base Combat Reach Multipliers")]
+        [Tooltip("Melee Reach Formula: ( reach = ( fCombatDistance | fCombatBashReach ) * NPCRaceScale * WeaponReach + ( fObjectHitWeaponReach | fObjectHitTwoHandReach | fObjectHitH2HReach ) )")]
+        public GameSettingsCombatReach CombatReach = new();
+        // Weapon type reach
+        [SettingName("Weapon Type Reach Modifiers")]
+        [Tooltip("Melee Reach Formula: ( reach = ( fCombatDistance | fCombatBashReach ) * NPCRaceScale * WeaponReach + ( fObjectHitWeaponReach | fObjectHitTwoHandReach | fObjectHitH2HReach ) )")]
+        public GameSettingsWeaponTypeReach WeaponTypeReach = new();
 
-		public int AddGameSettingsToPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
-		{
-			var count = 0;
-			count += WeaponTypeReach.AddGameSettings( state ); // add game settings from weapon type reach category
-			count += CombatReach.AddGameSettings( state ); // add game settings from combat reach category
-			return count;
-		}
-	}
+        public int AddGameSettingsToPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            var count = 0;
+            count += WeaponTypeReach.AddGameSettings(state); // add game settings from weapon type reach category
+            count += CombatReach.AddGameSettings(state); // add game settings from combat reach category
+            return count;
+        }
+    }
 }
